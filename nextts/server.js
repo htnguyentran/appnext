5a5e0b17cde50e5ee34a3c1ea0f15765fe135d9c
htnguyentran <haithong1993@gmail.com>
2022-04-13 16:49:54 +0700

    update

--- nextts/server.js
+++ nextts/server.js
@@ -3,16 +3,28 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
+// const port = process.env.PORT;
+
 // when using middleware `hostname` and `port` must be provided below
 const hostname = "localhost";
-const port = 3000;
+const port = 3001;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      if (pathname === "/a") {
+        await app.render(req, res, "/a", query);
+      } else if (pathname === "/b") {
+        await app.render(req, res, "/b", query);
+      } else {
+        await handle(req, res, parsedUrl);
+      }
     } catch (err) {
       console.error("Error occurred handling", req.url, err);
       res.statusCode = 500;
