f80982f6a39c40060a35df3aa398dd7e6e39a1f7
htnguyentran <haithong1993@gmail.com>
2022-05-05 00:08:35 +0700

    update env

--- nextts/next.config.js
+++ nextts/next.config.js
@@ -1,8 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
- const nextConfig = {
-  /* config options here */
-}
+const dotenv = require("dotenv");
+dotenv.config();
 
-module.exports = nextConfig
+const nextConfig = {
+  port: process.env.PORT,
+  hostname: process.env.HOST,
+  dev: process.env.NODE_ENV,
+  mongodb_url:process.env.MONGODB_URL
+};
+
+module.exports = nextConfig;
--- nextts/server.js
+++ nextts/server.js
@@ -1,31 +1,27 @@
 // server.js
-// const { createServer } = require("http");
-const { parse } = require("url");
 const next = require("next");
-const dev = process.env.NODE_ENV !== "production";
 const express = require("express");
-const server = express();
-const serverHttp = require("http").Server(server);
-
-// define .env.local
-const port = process.env.PORT ? "undefined" : 3001;
-const hostname = process.env.HOST ? "undefined" : "localhost";
+const { port, hostname, dev, mongodb_url } = require("./next.config");
 
 // check index mongodb
 //  repo(client).Indexes("doc", { "index": 1, "index": 1  }) search index numerical
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
-
 const middleware = require("./middleware/index");
 
 app.prepare().then(() => {
+
   const MongoClient = require("mongodb").MongoClient;
-  const client = new MongoClient(process.env.mongodb_url, {
+  const client_mongo = new MongoClient(mongodb_url, {
     useUnifiedTopology: true,
   });
 
+  const server = express();
+  const serverHttp = require("http").Server(server);
+
   server.get("/index", middleware.restrictAccess);
+  server.get("/register", middleware.restrictAccess);
 
   server.get("*", (req, res) => {
     return handle(req, res);
